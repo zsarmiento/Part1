f51e7fe79c549220078edf9621483c1eda7bdca6
Zulay Sarmiento <zulaysarmientoarevaloq@gmail.com>
2024-09-04 17:18:53 -0500

    entrega del ejercicio 1-5

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,30 @@ import { Header } from "./components/Header"
 import { Contents } from "./components/Contents"
 import { Total } from "./components/Total"
 const App = () => {
-  const course = 'Half Stack application development'
-  const partes = [
-  {
-   name:  'Fundamentals of React',
-   exercises: 10
-  },
-  
-  {
-  name: 'Using props to pass data',
-  exercises: 7
-  },
+  const course = {
+    name:'Half Stack application development',
+    partes : [
+              {
+              name:  'Fundamentals of React',
+              exercises: 10
+              },
+              
+              {
+              name: 'Using props to pass data',
+              exercises: 7
+              },
 
-  {
-    name: 'State of a component',
-    exercises: 14
-  }]
-
+              {
+                name: 'State of a component',
+                exercises: 14
+              }
+            ]
+          }
   return (
     <div>
-      <Header course={course} />
-      <Contents parts = {partes}  />
-      <Total totalExercises={partes}/>
+      <Header course={course.name} />
+      <Contents parts = {course.partes}  />
+      <Total totalExercises={course.partes}/>
     </div>
   )
 }
