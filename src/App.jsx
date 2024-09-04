bf7591f4a91cc3be2703ce862135c65b1a88ca38
Zulay Sarmiento <zulaysarmientoarevaloq@gmail.com>
2024-09-04 16:49:27 -0500

    se refactoriza codigo para utilizar objetos

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import { Contents } from "./components/Contents"
 import { Total } from "./components/Total"
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+   name:  'Fundamentals of React',
+   exercises: 10
+  }
+  
+  const part2 = {
+  name: 'Using props to pass data',
+  exercises: 7
+  }
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Contents parts = {[part1,part2,part3]} totalExercises = {[exercises1,exercises2,exercises3]} />
-      <Total totalExercises = {[exercises1,exercises2,exercises3]}/>
+      <Contents parts = {[part1.name,part2.name,part3.name]} totalExercises = {[part1.exercises,part2.exercises,part3.exercises]} />
+      <Total totalExercises = {[part1.exercises,part2.exercises,part3.exercises]}/>
     </div>
   )
 }
